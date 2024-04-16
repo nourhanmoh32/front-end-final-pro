25fd9705e1303e0e00f1d61b020eca8d3a85d424
nourhanmoh32 <nourhanm20001@gmail.com>
2024-04-17 00:57:12 +0200

    sec edit js

--- script.js
+++ script.js
@@ -1,24 +1,32 @@
-const username=document.getElementById("username");
+// password alerts
+const yourCode=document.getElementById("yourCode");
 const password=document.getElementById("password");
+const yourName=document.getElementById("yourName");
 
-form.addEventListener('submit',function(event){ 
-    if(username.value==="" || password.value===""){
+form1.addEventListener('submit',function(event){ 
+    if(yourCode.value==="" || password.value===""){
         alert("استكمل البيانات");
     }
     event.preventDefault();
 });
-const navLinks = document.querySelectorAll("nav a");
-navLinks.forEach(link => {
-    link.addEventListener("click", function(e){
-        e.preventDefault();
-
-        const sectionId =this.getAttribute("href");
-        const section =document.querySelector(sectionId);
-
-        const allSections = document.querySelectorAll("section");
-        allSections.forEach(section => {
-           section.style.display = "none";
-        });
-section.style.display = "block";
-    });
+form2.addEventListener('submit',function(event){ 
+    if(yourName.value==="" || yourCode.value===""){
+        alert("استكمل البيانات");
+    }
+    event.preventDefault();
 });
+
+// ----------------------------------------------------------------
+// slider login & sign up
+const a =document.getElementById("log-in");
+const b =document.getElementById("sign-up");
+
+function login(){
+    a.style.left="4px";
+    b.style.right="-510px";
+}
+function register(){
+    a.style.left="-510px";
+    b.style.right="4px";
+}
+// ---------------------------------------------------------------
